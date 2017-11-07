7041e1f5e251e525a4fdf2487144ffe61d753163
TheKrunch <mattcrunch@gmail.com>
2017-11-07 00:19:05 -0500

    added comments and changed some of the wording around

--- Python Stuff/HelloWorld+More.py
+++ Python Stuff/HelloWorld+More.py
@@ -1,10 +1,10 @@
-# This program says hello and asks for my name.
-print('Hello world!')
+# This program says hello and asks for the user's name.
+print('Hello World!')
 print('What is your name?')     # ask for their name
-myName = input()
-print('It is good to meet you, ' + myName + '.')
+myName = input()    # Capture name input into myName variable
+print('It is good to meet you, ' + myName + '.')    # uses myName to be more specific
 print('The length of your name is:')
-print(len(myName))
+print(len(myName))  # uses len() to find the lenghth of myName
 print('What is your age?')  # ask for their age
-myAge = input()
-print('You will be ' + str(int(myAge) + 1) + ' in a year.')
+myAge = input()     # Capture age input into myAge variable
+print('You will be ' + str(int(myAge) + 1) + ' in one year.')     # adds one to myAge
