ebc57cdf3633067893a92f1047b941ade84147c0
Matt <mattcrunch@gmail.com>
2020-01-17 02:24:16 -0500

    Implemented pathlib.Path

--- Python Stuff/list.py
+++ Python Stuff/list.py
@@ -1,43 +1,49 @@
-import os, time, datetime
+import os, time, subprocess
+from datetime import datetime
+from pathlib import Path
 
-#print("Using os.listdir() I get a list of strings of the files in the current directory.\n")
-#time.sleep(1)
-#dirlist = os.listdir()							# Creates a list of the files in the current directory as strings
-#print(dirlist, end='\n\n')
 
-print("Looping through current directory file names with os.scandir().\n")
-time.sleep(1)
+dirToScan = "E:/Game Highlights/OBS Recordings" # This is the directory I want to scan
+p = Path(dirToScan)                             # This converts the string to the directory into a Path Object
 
-with os.scandir() as curDir:					# os.scandir() makes an iterator of the files in the current directory
-	for entry in curDir:
-		if entry.is_file():						# Checks if the current object is a file
-			print(entry.name)					# If so it prints its file name
+print("This should print the file name and the date & time of creation for all the files in:", p, end='\n\n')
 
-print("\nOkay, now this should print the file name and date & time of file creation...\n")
+with os.scandir(p) as curDir:                   # Creates an iterator for the specified directory in p
+    for f in curDir:
+        if f.is_file():                         # Checks if its a file
+            curF = os.stat(f)                   # Saves the stat_result object into curF, this allows us to get file info
+            print(f.name, end=': ')             # Prints the current file's name
+            convTime = datetime.fromtimestamp(curF.st_ctime)       # This gets the ctime of the current file and formats it
+            print(convTime.strftime("%m/%d/%Y, %H:%M:%S"))         # This formats it again, not really sure why it has to be done twice
 
-time.sleep(1)
 
-with os.scandir() as curDir:					# Same as above creates the iterator
-	for f in curDir:
-		if f.is_file():							# Checks if its a file
-			curF = os.stat(f)					# Saves the stat_result object into curF, this allows us to get file info
-			print(f.name, end=': ')				# Prints the current file's name
-			convTime = datetime.datetime.fromtimestamp(curF.st_ctime)		# This gets the ctime of the current file and formats it
-			print(convTime.strftime("%m/%d/%Y, %H:%M:%S"))					# This formats it again, not really sure why it has to be done twice
+def getNewestFilePath():
+    with os.scandir(p) as pathDir:
+        newestFile = ''     # Where the newest file path will be stored
+        tempTime = 0        # Where the current newests file's time since the epoch is stored
 
-print("\nUp until this point I was using default parameter for path, now trying 'E:\\Game Highlights\\OBS Recordings' as path.")
+        for file in pathDir:
+            if file.is_file():
+                curFile = os.stat(file)             # Gets stats about the file and stores it in curFile
+                creatTime = curFile.st_ctime        # Gets how much times since the epoch it was when the file was created and stores it in creatTime
 
-time.sleep(1)
+                if creatTime > tempTime:
+                    newestFile = file.path
+                    tempTime = creatTime
+        
+    return newestFile
 
-with os.scandir('E:\\Game Highlights\\OBS Recordings') as pathDir:
-	for f in pathDir:
-		if f.is_file():
-			curF = os.stat(f)
-			print(f.name, end='\n')
+print("")
+print("This should print the path to the newest file.\n")
+newPath = Path(getNewestFilePath())
+print(newPath)
 
 # The following line should be run with subprocess.Popen() (or subprocess.run() not sure), it returns the duration of the clip in seconds.
 # ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 "FILENAMEHERE"
 # FILENAMEHERE can also be a path to the file, or just the file name itself, depending on where script is executing.
+
+#args = ['ffprobe', '-v', 'error' , '-show_entries', 'format=duration', '-of', 'default=noprint_wrappers=1:nokey=1', '"FILENAMEHERE"']
+#ffprobeProc = subprocess.run(args, stdout=subprocess.PIPE)
 
 # After the file probe get the output duration in seconds using subprocess.*() stuff,
 # Using calculated elapsed time and duration of clip, (btw read "Clip audio script idea.txt")
